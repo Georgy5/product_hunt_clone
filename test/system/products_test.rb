677ccde4b9358666c4e990fcbb03649c67d9dd9e
Karl Keller <kgk86@hotmail.com>
2021-02-22 20:09:07 +0100

    update the name/description of the test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the homepage with products index" do
     visit products_url
   
     assert_selector "h1", text: "Awesome Products"
