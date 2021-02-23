b63f6c8b789600b77827ece8f8e042828e82b804
Karl Keller <kgk86@hotmail.com>
2021-02-24 00:00:14 +0100

    update 'create' action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,17 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new
-    @product.save
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
   end
 end
