146ecf56afde5d0b8d6abb19f21fc2ed85fc92ab
Karl Keller <kgk86@hotmail.com>
2021-02-23 14:41:47 +0100

    create test for adding a new product

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -7,4 +7,18 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Awesome Products"
     assert_selector ".card-product", count: Product.count
   end
+
+  test "let a signed in user create a new product" do
+    login_as users(:goerge)
+    # visit '/products/new'
+    visit new_product_path
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: learn to code"
+
+    click_on 'Create product'
+
+    assert_equal root_path, page.current_path
+    assert_text "Change your life: learn to code"
+  end
 end
